cf9bb65d74a58ecdafce861e2a88dfd3edf7977c
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-08-29 07:14:01 +0700

    Fix deprecated { useNewUrlParser: true }

--- configs/loadModelsMongoose.js
+++ configs/loadModelsMongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const fs = require('fs')
 const path = require('path')
 
-mongoose.connect(config.uriMongo)
+mongoose.connect(config.uriMongo, { useNewUrlParser: true })
 
 const modelsPath = path.resolve(__dirname, '../models')
 fs.readdirSync(modelsPath).forEach(file => {
