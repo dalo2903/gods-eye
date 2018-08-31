498de351d2214f589b8299709192013e912f2c7c
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-09-01 06:55:30 +0700

    Draw rectangle

--- public/javascripts/post/detail.js
+++ public/javascripts/post/detail.js
@@ -5,8 +5,45 @@ app.controller('detailController', ['$scope', 'apiService', function ($scope, ap
   apiService.getPost(_id)
     .then(function (res) {
       $scope.post = res.data.post
+
+      // $scope.post.datas.forEach(function (data, index) {
+      //   if (data.identifyResult && data.identifyResult.persons.length) {
+      //     const facerectangle = data.identifyResult.persons[0].facerectangle
+      //     $('#box-' + index).css({
+      //       top: facerectangle.top + 'px',
+      //       left: facerectangle.left + 'px'
+      //     })
+      //   }
+      // })
     })
     .catch(function (res) {
       console.log(res)
     })
 }])
+
+// function drawRectangle () {
+//   // var c = document.getElementById('myCanvas')
+//   // var ctx = c.getContext('2d')
+
+//   // // var offset = $('img').offset()
+//   // // Red rectangle
+//   // ctx.beginPath()
+//   // ctx.lineWidth = '6'
+//   // ctx.strokeStyle = 'red'
+//   // ctx.rect(5, 5, 201, 201)
+//   // ctx.stroke()
+
+//   var stage = new Facade(document.querySelector('#myCanvas'))
+//   var rect = new Facade.Rect({
+//     x: 0,
+//     y: 0,
+//     width: 201,
+//     height: 201,
+//     lineWidth: 6,
+//     strokeStyle: '#333E4B',
+//     fillStyle: '#1C73A8',
+//     anchor: 'top/left'
+//   })
+
+//   stage.addToStage(rect)
+// }
