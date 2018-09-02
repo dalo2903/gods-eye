dce7de6b8344f046105b83ab09a37f06cee14cd4
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-09-02 14:41:30 +0700

    API get posts by location

--- api/routes/location.js
+++ api/routes/location.js
@@ -1,10 +1,22 @@
 const express = require('express')
 let router = express.Router()
 const LocationController = require('../controllers/LocationController')
+const PostController = require('../controllers/PostController')
 
 router.get('/', async (req, res) => {
   try {
     const response = await LocationController.getLocations()
+    return res.send(response)
+  } catch (error) {
+    console.log(error)
+    return res.status(error.status || 500).send(error)
+  }
+})
+
+router.get('/:locationId/posts', async (req, res) => {
+  try {
+    const locationId = req.params.locationId
+    const response = await PostController.getPostsByLocation(locationId)
     return res.send(response)
   } catch (error) {
     console.log(error)
