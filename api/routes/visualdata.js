eae7d72772ea913cf83f6f098010d604e5fb35f3
dalo2903 <dalo2903@gmail.com>
2018-11-01 14:53:36 +0700

    VIsual data change

--- api/routes/visualdata.js
+++ api/routes/visualdata.js
@@ -4,6 +4,10 @@ const VisualDataController = require('../controllers/VisualDataController')
 const AuthService = require('../services/AuthService')
 const DatasetCollector = require('../controllers/DatasetCollector')
 const UploadController = require('../controllers/UploadController')
+const PostController = require('../controllers/PostController')
+const IdentifyController = require('../controllers/IdentifyController')
+const constants = require('../../configs/constants')
+const responseStatus = require('../../configs/responseStatus')
 
 router.get('/label', async (req, res) => {
   try {
@@ -50,11 +54,59 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.post('/classified', async (req, res) => {
+  try {
+    console.log(req.body)
+    const info = req.body
+    // const file = req.body.file
+    var analyzeData = []
+    const userCreated = info.id
+    if (userCreated !== constants.adminInfo.id) {
+      throw responseStatus.Response(403, {}, responseStatus.WRONG_EMAIL_OR_PASSWORD)
+    }
+    const visualDataId = info.video_id
+    const location = VisualDataController.get(visualDataId).location
+    
+    req.body.datas = []
+    for (let i in req.files) {
+      console.log(i)
+      // const isImage = req.files[i].type.startsWith('image')
+      // console.log(isImage)
+      var visualData = await VisualDataController.createVisualData({
+        URL: 'https://vignette.wikia.nocookie.net/mixels/images/f/f4/No-image-found.jpg',
+        isImage: true
+      })
+      console.log(visualData._id)
+      const url = await UploadController.uploadFileV3(req.files[i], visualData._id)
+      visualData.URL = url
+      await visualData.save()
+      req.body.datas.push(visualData._id)
+      analyzeData.push({
+        url: url,
+        id: visualData._id
+      })
+    }
+    req.body.title = 'Suspicious behaviour detected'
+    req.body.location = location
+    const post = await PostController.createPost(req.body, userCreated)
+    for (let data of analyzeData) {
+      const analyzeAndProcessResponse = await IdentifyController.analyzeAndProcessFaces(data.url, location, post._id, data.id)
+      console.log(analyzeAndProcessResponse.persons)
+      await VisualDataController.updateIdentifyResult(data.id, analyzeAndProcessResponse.persons)
+    }
+    return res.sendStatus(200)
+  } catch (error) {
+    // console.log(error)
+    return res.status(error.status || 500).send(error)
+  }
+})
 router.post('/', async (req, res) => {
   try {
-
     console.log(req.body)
+    const info = req.body
     // const file = req.body.file
+    var analyzeData = []
+    req.body.datas = []
     for (let i in req.files) {
       console.log(i)
       // const isImage = req.files[i].type.startsWith('image')
@@ -68,12 +120,10 @@ router.post('/', async (req, res) => {
       visualData.URL = url
       await visualData.save()
     }
-
     return res.sendStatus(200)
   } catch (error) {
     // console.log(error)
     return res.status(error.status || 500).send(error)
   }
 })
-
 module.exports = router
