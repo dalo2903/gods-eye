d69a658b0d9736b54438e84d1168b355fb5e4f3e
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-11-15 10:23:12 +0700

    Update Admin Dashboard UI

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -11,28 +11,64 @@ app.controller('AdminController', ['$scope', 'apiService', function ($scope, api
     })
   // Visual data
   $('#visualTable').DataTable({
-    "ajax": '/api/visual-data',
-    "columns": [
-      { "data": "URL" },
-      { "data": "location.name" },
-      { "data": "createdAt" },
-      { "data": "identifyResult.persons[<br>].personId.name" }
+    'ajax': '/api/visual-data',
+    'columns': [
+      { data: 'URL' },
+      {
+        data: 'location.name',
+        defaultContent: '<i>Not available</i>'
+      },
+      { data: 'createdAt' },
+      { data: 'identifyResult.persons[<br>].personId.name' }
     ],
     fnCreatedRow: function (row, data, index) {
-      $('td', row).eq(0).html('<a style="color:blue" href="' + data.URL + '"> <img src="' + data.URL + '"></a>');
-    }
-  });
-  //Record
+      if (data.isImage) {
+        $('td', row).eq(0).html('<a style="color:blue" href="' + data.URL + '"> <img class="visual-data" src="' + data.URL + '"></a>')
+      } else {
+        $('td', row).eq(0).html('<a style="color:blue" href="' + data.URL + '"> <video class="visual-data" controls><source src="' + data.URL + '" type="video/mp4" ></video></a>')
+      }
+    },
+    columnDefs: [
+      {
+        targets: 2,
+        render: function (data) {
+          return moment(data).format('HH:mm DD/MM/YYYY')
+        }
+      }
+    ]
+  })
+  // Record
   $('#recordTable').DataTable({
-    "ajax": '/api/record',
-    "columns": [
-      { "data": "URL" },
-      { "data": "location.name" },
-      { "data": "createdAt" },
-      { "data": "identifyResult.persons[<br>].personId.name" }
+    'ajax': '/api/record',
+    'columns': [
+      {
+        data: 'URL',
+        defaultContent: '<img class="visual-data" src="https://vignette.wikia.nocookie.net/mixels/images/f/f4/No-image-found.jpg"></a>'
+      },
+      {
+        data: 'location.name',
+        defaultContent: '<i>Not available</i>'
+      },
+      { data: 'createdAt' },
+      {
+        data: 'identifyResult.persons[<br>].personId.name',
+        defaultContent: '<i>Not available</i>'
+      }
     ],
     fnCreatedRow: function (row, data, index) {
-      $('td', row).eq(0).html('<a style="color:blue" href="' + data.URL + '"> <img src="' + data.URL + '"></a>');
-    }
-  });
+      if (data.isImage) {
+        $('td', row).eq(0).html('<a style="color:blue" href="' + data.URL + '"> <img class="visual-data" src="' + data.URL + '"></a>')
+      } else {
+        $('td', row).eq(0).html('<a style="color:blue" href="' + data.URL + '"> <video class="visual-data" controls><source src="' + data.URL + '" type="video/mp4" ></video></a>')
+      }
+    },
+    columnDefs: [
+      {
+        targets: 2,
+        render: function (data) {
+          return moment(data).format('HH:mm DD/MM/YYYY')
+        }
+      }
+    ]
+  })
 }])
