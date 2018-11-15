61933f5fa05022b2a8ab8772ab27ae1b5b5490d9
Vu Minh Tri <minhtrivu2611@gmail.com>
2018-11-15 13:47:18 +0700

    làm xong trang subscription

--- public/javascripts/subscription.js
+++ public/javascripts/subscription.js
@@ -19,13 +19,14 @@ app.controller('SubscriptionController', ['$scope', '$http', '$compile', functio
         ],
         createdRow: function (row, data, index) {
             if (data.subscribers.includes(userId)) {
-                $('td', row).eq(2).html('<button id="unsubscribeButton" class="btn btn-warning" ng-click="unsubscribe(' + data._id + ')">Unsubscribe</button>');
+                $('td', row).eq(2).html('<button id="unsubscribeButton' + data._id + '" class="btn btn-warning" ng-click="unsubscribe(\'' + data._id + '\')">Unsubscribe</button>' +
+                    '<button id="subscribeButton' + data._id + '" class="btn btn-primary hide" ng-click="subscribe(\'' + data._id + '\')">Subscribe</button>');
                 $compile(angular.element(row).contents())($scope);
             }
             else {
-                $('td', row).eq(2).html('<button id="subscribeButton" class="btn btn-primary" ng-click="subscribe(\'' + data._id + '\')">Subscribe</button>');
+                $('td', row).eq(2).html('<button id="subscribeButton' + data._id + '" class="btn btn-primary" ng-click="subscribe(\'' + data._id + '\')">Subscribe</button>' +
+                    '<button id="unsubscribeButton' + data._id + '" class="btn btn-warning hide" ng-click="unsubscribe(\'' + data._id + '\')">Unsubscribe</button>');
                 $compile(angular.element(row).contents())($scope);
-
             }
         }
     });
@@ -34,12 +35,23 @@ app.controller('SubscriptionController', ['$scope', '$http', '$compile', functio
             method: 'GET',
             url: '/api/location/' + locationId + '/subscribe'
         }).then(function successCallback(response) {
-            console.log("abc")
+            alert("Subscribed")
+            $('#subscribeButton' + locationId).addClass('hide')
+            $('#unsubscribeButton' + locationId).removeClass('hide')
         }, function errorCallback(response) {
             console.log(response)
         });
     }
     $scope.unsubscribe = function (locationId) {
-        console.log("12412")
+        $http({
+            method: 'GET',
+            url: '/api/location/' + locationId + '/unsubscribe'
+        }).then(function successCallback(response) {
+            alert("Unsubscribed")
+            $('#subscribeButton' + locationId).removeClass('hide')
+            $('#unsubscribeButton' + locationId).addClass('hide')
+        }, function errorCallback(response) {
+            console.log(response)
+        });
     }
 }])
