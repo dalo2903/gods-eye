337d0e9592c84ed5137c41986697e283b7700894
Nam Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-11-16 09:28:09 +0700

    Show error modal when log in

--- public/javascripts/signin.js
+++ public/javascripts/signin.js
@@ -1,21 +1,21 @@
 var app = angular.module('GodsEye')
 
 app.controller('SignInController', ['$scope', 'apiService', function ($scope, apiService) {
-  $scope.signUp = function () {
-    const json = {
-      name: $('#signupForm input[name=name]').val().trim(),
-      email: $('#signupForm input[name=email]').val().trim(),
-      password: $('#signupForm input[name=password]').val().trim()
-    }
-    apiService.signUp(json)
-      .then(function (res) {
-        alert(res.data.message)
-        //window.location.href = '/'
-      })
-      .catch(function (res) {
-        alert(res.data.message)
-      })
-  }
+  // $scope.signUp = function () {
+  //   const json = {
+  //     name: $('#signupForm input[name=name]').val().trim(),
+  //     email: $('#signupForm input[name=email]').val().trim(),
+  //     password: $('#signupForm input[name=password]').val().trim()
+  //   }
+  //   apiService.signUp(json)
+  //     .then(function (res) {
+  //       alert(res.data.message)
+  //       // window.location.href = '/'
+  //     })
+  //     .catch(function (res) {
+  //       alert(res.data.message)
+  //     })
+  // }
 
   $scope.signIn = function () {
     const json = {
@@ -32,7 +32,8 @@ app.controller('SignInController', ['$scope', 'apiService', function ($scope, ap
         window.location.href = '/'
       })
       .catch(function (res) {
-        alert(res.data.message)
+        // alert(res.data.message)
+        showErrorModal(res.data.message)
       })
   }
 }])
