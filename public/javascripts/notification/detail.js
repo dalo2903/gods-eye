776862012cd52df223cfe8a2e94b53b6abcaf906
Nam Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-11-17 09:56:39 +0700

    Display video in notification detail

--- public/javascripts/notification/detail.js
+++ public/javascripts/notification/detail.js
@@ -4,11 +4,13 @@ app.controller('detailController', ['$scope', 'apiService', function ($scope, ap
   const _id = $('#_id').text().trim()
   apiService.getNotification(_id)
     .then(function (res) {
-      console.log(res)
+      // console.log(res)
       $scope.notification = res.data.notification
+      $scope.foo = true
+      // https://stackoverflow.com/a/38107284
     })
     .catch(function (res) {
-      console.log(res)
+      // console.log(res)
     })
 }])
 
