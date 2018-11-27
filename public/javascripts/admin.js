73e9e7f7d4c267914776790af455fb6cd4dc56dc
Vu Minh Tri <minhtrivu2611@gmail.com>
2018-11-27 14:55:19 +0700

    draft

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -71,4 +71,34 @@ app.controller('AdminController', ['$scope', 'apiService', function ($scope, api
       }
     ]
   })
+  //Pending posts
+  $scope.scrollData = []
+  let last = 0
+  $scope.block = false
+  function unique(a) {
+    var seen = {}
+    var out = []
+    var len = a.length
+    var j = 0
+    for (var i = 0; i < len; i++) {
+      var item = a[i]._id
+      if (seen[item] !== 1) {
+        seen[item] = 1
+        out[j++] = a[i]
+      }
+    }
+    return out
+  }
+  $scope.loadMore = async function () {
+    $scope.block = true
+    last = $scope.scrollData.length - 1
+    const newPosts = (await apiService.getPosts(last + 1, 5)).data.posts
+    // for (var i = last + 1; i < last + 3; i++) {
+    //   $scope.scrollData.push(posts[i])
+    // }
+    $scope.scrollData = $scope.scrollData.concat(newPosts)
+    $scope.scrollData = unique($scope.scrollData)
+    $scope.block = false
+    $scope.$apply()
+  }
 }])
