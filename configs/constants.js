80f6e8c14403997333cf514e56ba24df0f178698
Nam Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-12-01 17:47:12 +0700

    Update for Facebook Open Graph API

--- configs/constants.js
+++ configs/constants.js
@@ -1,7 +1,7 @@
 const config = require('../config')
 
 const index = {
-  image: config.address + '/favicon.ico',
+  image: config.address + '/favicon.png',
   description: 'If you’re someone who is routinely misplacing or losing things like the TV remote, your keys, wallet, or smartphone, then this app is created for you. GE is an app that can identify objects, match and link existing data from open data sources (images, videos, audio files are shared publicly by users) to search for an object. This can be applied to tracing lost items, lost or missing person, and improve security in the Smart City.',
   title: 'God\'s Eye',
   type: 'website',
