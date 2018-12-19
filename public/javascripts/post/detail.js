c85fb7b509105b34ce244c25ca7ed7d8620fa4fa
Vu Minh Tri <minhtrivu2611@gmail.com>
2018-12-19 16:02:52 +0700

    them report vao detail post

--- public/javascripts/post/detail.js
+++ public/javascripts/post/detail.js
@@ -2,10 +2,26 @@ var app = angular.module('GodsEye')
 
 app.controller('detailController', ['$scope', 'apiService', function ($scope, apiService) {
   const _id = $('#_id').text().trim()
+  $scope.report = function (postId) {
+    var check = confirm('Report post for rules violation?')
+    if (check == true) {
+      $http({
+        method: 'put',
+        url: '/api/post/' + postId + '/report'
+      }).then(function successCallback(response) {
+        $('#reportButton' + postId).attr('disabled', 'disabled')
+        $('#reportButton' + postId).removeClass('btn-danger')
+        $('#reportButton' + postId).html('Reported')
+      }, function errorCallback(response) {
+        console.log(response)
+      })
+    }
+  }
   apiService.getPost(_id)
     .then(function (res) {
       $scope.post = res.data.post
     })
     .catch(function () {
     })
+
 }])
